68027c72237371dbcd42e98c02d84f3ecfc05096
shafiulla70 <132994542+shafiulla70@users.noreply.github.com>
2023-08-28 19:52:50 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenGamingToken is ERC20, Ownable {
+    constructor() ERC20("Degen", "DGN") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint initial tokens
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than 0");
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+        _mint(to, amount);
+    }
+
+  
+    function transferTokens(address to, uint256 amount) public {
+        require(to != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than 0");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _transfer(msg.sender, to, amount);
+    }
+
+
+    function redeemTokens(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _burn(msg.sender, amount);
+    }
+
+
+    function burnTokens(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _burn(msg.sender, amount);
     }
 }
